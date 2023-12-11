7751c6e6c1834c1a98fea5a376ab6597dbeb90dd
Alexis Lucyk <alexislucyk@gmail.com>
2023-12-10 23:20:45 -0300

    first commit

---
+++ vistas/dashboard.php
@@ -0,0 +1,122 @@
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Principal</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+                    <li class="breadcrumb-item active">Principal</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+    <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h4 class="totalProductos">125</h4>
+
+                        <p>Productos Registrados</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="corsor:pointer;" class="small-box-footer">Mas Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <!-- TARJETA TOTAL COMPRAS -->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <!--  con la clase bg-success le da color verde a la card -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h4 class="totalCompras">$ 2.500</h4>
+
+                        <p>Total Compras</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-cash"></i>
+                    </div>
+                    <a style="corsor:pointer;" class="small-box-footer">Mas Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- TARJETA TOTAL VENTAS -->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h4 class="totalVentas">$ 12.125</h4>
+
+                        <p>Total Ventas</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="corsor:pointer;" class="small-box-footer">Mas Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h4 class="totalGanancias">$ 32.125</h4>
+
+                        <p>Total Ganancias</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="corsor:pointer;" class="small-box-footer">Mas Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- TARJETA POCO STOCK -->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-primary">
+                    <div class="inner">
+                        <h4 class="totalProductosMinStock">12</h4>
+
+                        <p>Productos Poco Stock</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="corsor:pointer;" class="small-box-footer">Mas Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            
+            <!-- TARJETA VENTAS DIA -->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-secondary">
+                    <div class="inner">
+                        <h4 class="totalVentaHoy">$ 8.102</h4>
+
+                        <p>Ventas de dia</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="corsor:pointer;" class="small-box-footer">Mas Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content -->
